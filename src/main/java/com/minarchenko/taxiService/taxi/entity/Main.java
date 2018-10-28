c16f864e99852a579c9b7a3ec61c9b89455af915
Natasha13 <natas_ha@ua.fm>
2018-10-28 21:35:40 +0200

    Taxi without tests

--- src/main/java/com/minarchenko/taxiService/taxi/entity/Main.java
+++ src/main/java/com/minarchenko/taxiService/taxi/entity/Main.java
@@ -2,20 +2,51 @@ package com.minarchenko.taxiService.taxi.entity;
 
 import com.minarchenko.taxiService.DataSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String csvFileCar = "C:/NATA/Java/Courses/EPAM/TaxiService/src/main/resources/cars.csv";
         String csvFileTruck = "C:/NATA/Java/Courses/EPAM/TaxiService/src/main/resources/truck.csv";
-        DataSource obj = new DataSource();
-        List<Car> allCars = obj.getCars(csvFileCar);
-        List<Truck> allTrucks = obj.getTrucks(csvFileTruck);
+
+        DataSource dataSource = new DataSource();
+        List<Car> allCars = dataSource.getCars(csvFileCar);
+        List<Truck> allTrucks = dataSource.getTrucks(csvFileTruck);
+        List<TaxiEntity> taxiEntities=new ArrayList<>();
+        taxiEntities.addAll(allCars);
+        taxiEntities.addAll(allTrucks);
+
+        int taxiValueSum=0;
+
         for (Truck allTruck : allTrucks) {
             System.out.println(allTruck);
+            taxiValueSum=taxiValueSum+allTruck.getValue();
         }
+
         for (Car allCar : allCars) {
             System.out.println(allCar);
+            taxiValueSum=taxiValueSum+allCar.getValue();
+        }
+
+        System.out.println();
+        for (TaxiEntity taxiEntity: taxiEntities) {
+            System.out.println(taxiEntity);
+        }
+
+        System.out.println("");
+        System.out.println("Sum = "+taxiValueSum);
+
+        System.out.println();
+
+        for (TaxiEntity taxiEntity: TaxiService.sortFuel(taxiEntities)) {
+            System.out.println(taxiEntity);
+        }
+
+        System.out.println();
+        System.out.println("Speed between 120 and 150");
+        for (TaxiEntity taxiEntity: TaxiService.speedRange(taxiEntities,120,150)) {
+            System.out.println(taxiEntity);
         }
     }
 }
---
+++ src/main/java/com/minarchenko/taxiService/taxi/entity/TaxiService.java
@@ -0,0 +1,31 @@
+package com.minarchenko.taxiService.taxi.entity;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TaxiService {
+    public static double sumValue(List<TaxiEntity> taxiEntity) {
+        int resultSum=0;
+        for (TaxiEntity taxiEntity1 : taxiEntity) {
+            resultSum = resultSum + taxiEntity1.getValue();
+        }
+        return resultSum;
+    }
+
+    public static List<TaxiEntity> sortFuel(List<TaxiEntity> taxiEntity){
+        List<TaxiEntity> taxiEntity1=new ArrayList<>(taxiEntity);
+        taxiEntity1.sort(Comparator.comparing(TaxiEntity::getFuel));
+        return taxiEntity1;
+    }
+
+    public static List<TaxiEntity> speedRange(List<TaxiEntity> taxiEntity, double minValue, double maxValue){
+        List<TaxiEntity> taxiEntityNew=new ArrayList<>();
+        for (TaxiEntity taxiEntity1 : taxiEntity) {
+          if(taxiEntity1.getSpeed()<maxValue&&taxiEntity1.getSpeed()>minValue){
+              taxiEntityNew.add(taxiEntity1);
+          }
+        }
+        return taxiEntityNew;
+    }
+}
